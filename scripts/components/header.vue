e9beeb283e59b725947551dac11bb51ea5490003
sumeet <you@example.com>
2018-11-30 18:28:40 +0530

    adding uxg logo link from uxg wordpress

--- scripts/components/header.vue
+++ scripts/components/header.vue
@@ -1,7 +1,7 @@
  const headerView = {
 	 template:`<nav class="navbar navbar-light bg-light">
 	 <div id="logo-after">
-	 <a class="navbar-brand" href="#"><img src="images/logo.png" alt="UXGorilla" id="logo"></a>  
+	 <a class="navbar-brand" href="#"><img src="https://uxgorilla.com/wp-content/uploads/2018/05/120X120.png" alt="UXGorilla" id="logo"></a>  
 	 </div>
 	 
 	 </nav>`
